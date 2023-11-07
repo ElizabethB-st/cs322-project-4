06bd7def2b75d39b1e9d749602e850ffd43653a2
ebowden05 <embowden05@gmail.com>
2023-11-06 22:34:50 -0800

    completed requirements

--- brevets/acp_times.py
+++ brevets/acp_times.py
@@ -14,124 +14,141 @@ import math
 #  same arguments.
 
 # create dictionarys for the specified time and location values
-
-# controls between 0-200 km
 control_0_2 = {'start_dist': 0, 'end_dist': 200, 'min_speed': 15, 'max_speed': 34}
-
-# controls between 200-400 km
 control_2_4 = {'start_dist': 200, 'end_dist': 400, 'min_speed': 15, 'max_speed': 32}
-
-# controls between 400-600 km
 control_4_6 = {'start_dist': 400, 'end_dist': 600, 'min_speed': 15, 'max_speed': 30}
-
-# controls between 600-1000 km
 control_6_10 = {'start_dist': 600, 'end_dist': 1000, 'min_speed': 11.428, 'max_speed': 28}
 
 
-def control_st_calc(control, brevet_start_time, max_speed):
+def control_st_calc(control, max_speed):
    raw_time = control / max_speed
    hour = math.floor(raw_time)
    frac_minutes = raw_time - hour
    minute = round(frac_minutes * 60)
-   return brevet_start_time.shift(hours=hour, minutes=minute)
+   return_val = {'hour': hour, 'minute': minute}
+   return return_val
 
-def control_cl_calc(control, brevet_start_time, min_speed):
+def control_cl_calc(control, min_speed):
    raw_time = control / min_speed
    hour = math.floor(raw_time)
    frac_minutes = raw_time - hour
    minute = round(frac_minutes * 60)
-   return brevet_start_time.shift(hours=hour, minutes=minute)
+   return_val = {'hour': hour, 'minute': minute}
+   return return_val
 
 def open_time(control_dist_km, brevet_dist_km, brevet_start_time):
-    """
-    Args:
-       control_dist_km:  number, control distance in kilometers
-       brevet_dist_km: number, nominal distance of the brevet
-           in kilometers, which must be one of 200, 300, 400, 600,
-           or 1000 (the only official ACP brevet distances)
-       brevet_start_time:  An arrow object
-    Returns:
-       An arrow object indicating the control open time.
-       This will be in the same time zone as the brevet start time.
-    """
-    start_time = brevet_start_time
+   """
+   Args:
+      control_dist_km:  number, control distance in kilometers
+      brevet_dist_km: number, nominal distance of the brevet
+         in kilometers, which must be one of 200, 300, 400, 600,
+         or 1000 (the only official ACP brevet distances)
+      brevet_start_time:  An arrow object
+   Returns:
+      An arrow object indicating the control open time.
+      This will be in the same time zone as the brevet start time.
+   """
+   time = arrow.get(brevet_start_time)
+   hour = 0
+   minute = 0
 
    #if the control is longer than the brevet the calulation should be with brevet instead
-    if control_dist_km > brevet_dist_km:
-       control = brevet_dist_km
-    else:
-       control = control_dist_km
-        
-    if control <= 200:
-       start_time = control_st_calc(control, brevet_start_time, control_0_2['max_speed'])
-    elif control <= 400:
-       #calcalute when distance is <=200
-       new_start_time = control_st_calc(200, brevet_start_time, control_0_2['max_speed'])
+   if control_dist_km > brevet_dist_km:
+      control = brevet_dist_km
+   else:
+      control = control_dist_km
+      
+   if control <= 200:
+      time_result = control_st_calc(control, control_0_2['max_speed'])
+      hour = time_result['hour']
+      minute = time_result['minute']
+   elif control <= 400:
+      #calcalute when distance is <=200
+      time_result_1 = control_st_calc(200, control_0_2['max_speed'])
+   
+      #calculate when distance is >200
+      remain_control_dist = control - 200
+      time_result_2 = control_st_calc(remain_control_dist, control_2_4['max_speed'])
 
-       #calculate when distance is >200 with new start time
-       remain_control_dist = control - 200
-       start_time = control_st_calc(remain_control_dist, new_start_time, control_2_4['max_speed'])
-    elif control <= 600:
-       #calcalute when distance is <=200
-       new_start_time = control_st_calc(200, brevet_start_time, control_0_2['max_speed'])
+      hour = time_result_1['hour'] + time_result_2['hour']
+      minute = time_result_1['minute'] + time_result_2['minute']
+   elif control <= 600:
+      #calcalute when distance is <=200
+      time_result_1 = control_st_calc(200, control_0_2['max_speed'])
 
-       #calculate when distance is <=400 with new start time
-       new_start_time = control_st_calc(200, new_start_time, control_2_4['max_speed'])
+      #calculate when distance is <=400
+      time_result_2 = control_st_calc(200, control_2_4['max_speed'])
 
-       #calulate remaining distance with new start time
-       remain_control_dist = control - 400
-       start_time = control_st_calc(remain_control_dist, new_start_time, control_4_6['max_speed'])
-    elif control <= 1000:
-       #calcalute when distance is <=200
-       new_start_time = control_st_calc(200, brevet_start_time, control_0_2['max_speed'])
+      #calulate remaining distance with new start time
+      remain_control_dist = control - 400
+      time_result_3 = control_st_calc(remain_control_dist, control_4_6['max_speed'])
+      
+      hour = time_result_1['hour'] + time_result_2['hour'] + time_result_3['hour']
+      minute = time_result_1['minute'] + time_result_2['minute'] + time_result_3['minute']
+   elif control <= 1000:
+      #calcalute when distance is <=200
+      time_result_1 = control_st_calc(200, control_0_2['max_speed'])
 
-       #calculate when distance is <=400
-       new_start_time = control_st_calc(200, new_start_time, control_2_4['max_speed'])
+      #calculate when distance is <=400
+      time_result_2 = control_st_calc(200, control_2_4['max_speed'])
 
-       #calulate when distance is <=600
-       new_start_time = control_st_calc(200, new_start_time, control_4_6['max_speed'])
-       
-       #calulate remaing distance
-       remain_control_dist = control - 600
-       start_time = control_st_calc(remain_control_dist, new_start_time, control_6_10['max_speed'])
+      #calulate when distance is <=600
+      time_result_3 = control_st_calc(200, control_4_6['max_speed'])
+      
+      #calulate remaing distance
+      remain_control_dist = control - 600
+      time_result_4 = control_st_calc(remain_control_dist, control_6_10['max_speed'])
+      hour = time_result_1['hour'] + time_result_2['hour'] + time_result_3['hour'] + time_result_4['hour']
+      minute = time_result_1['minute'] + time_result_2['minute'] + time_result_3['minute'] + time_result_4['minute']
 
-    return start_time
+   start_time = time.shift(hours=hour, minutes=minute)
+   return start_time
 
 
 def close_time(control_dist_km, brevet_dist_km, brevet_start_time):
-    """
-    Args:
-       control_dist_km:  number, control distance in kilometers
-          brevet_dist_km: number, nominal distance of the brevet
-          in kilometers, which must be one of 200, 300, 400, 600, or 1000
-          (the only official ACP brevet distances)
-       brevet_start_time:  An arrow object
-    Returns:
-       An arrow object indicating the control close time.
-       This will be in the same time zone as the brevet start time.
-    """
-    close_time = brevet_start_time
-     #if the control is longer than the brevet the calulation should be with brevet instead
-    if control_dist_km > brevet_dist_km:
-       control = brevet_dist_km
-    else:
-       control = control_dist_km
+   """
+   Args:
+      control_dist_km:  number, control distance in kilometers
+         brevet_dist_km: number, nominal distance of the brevet
+         in kilometers, which must be one of 200, 300, 400, 600, or 1000
+         (the only official ACP brevet distances)
+      brevet_start_time:  An arrow object
+   Returns:
+      An arrow object indicating the control close time.
+      This will be in the same time zone as the brevet start time.
+   """
+   time = arrow.get(brevet_start_time)
+   hour = 0
+   minute = 0
 
-    #if the control is 0 close time should be 1hr
-    if control == 0:
-       close_time = brevet_start_time.shift(hours=1)
-    elif control <= 60:
-       close_time = control_st_calc(control, brevet_start_time, 20)
-       close_time = close_time.shift(hours=1)
-    elif control == 200:
-       close_time = brevet_start_time.shift(hours=13, minutes=30)
-    elif control <= 600:
-       close_time = control_st_calc(control, brevet_start_time, control_0_2['min_speed'])
-    elif control <= 1000:
-       #calcalute when distance is <=600
-       new_start_time = control_st_calc(600, brevet_start_time, control_4_6['min_speed'])
-       #calulate remaining distance with new start time
-       remain_control_dist = control - 600
-       close_time = control_st_calc(remain_control_dist, new_start_time, control_6_10['min_speed'])
+   #if the control is longer than the brevet the calulation should be with brevet instead
+   if control_dist_km > brevet_dist_km:
+      control = brevet_dist_km
+   else:
+      control = control_dist_km
 
-    return close_time
+   #if the control is 0 close time should be 1hr
+   if control == 0:
+      hour = 1
+   elif control <= 60:
+      time_result = control_st_calc(control, 20)
+      hour = time_result['hour'] + 1
+      minute = time_result['minute']
+   elif control == 200:
+      hour = 13
+      minute = 30
+   elif control <= 600:
+      time_result = control_st_calc(control, control_0_2['min_speed'])
+      hour = time_result['hour']
+      minute = time_result['minute']
+   elif control <= 1000:
+      #calcalute when distance is <=600
+      time_result_1 = control_st_calc(600, control_4_6['min_speed'])
+      #calulate remaining distance with new start time
+      remain_control_dist = control - 600
+      time_result_2 = control_st_calc(remain_control_dist, control_6_10['min_speed'])
+      hour = time_result_1['hour'] + time_result_2['hour']
+      minute = time_result_1['minute'] + time_result_2['minute']
+
+   close_time = time.shift(hours=hour, minutes=minute)
+   return close_time
--- brevets/flask_brevets.py
+++ brevets/flask_brevets.py
@@ -47,18 +47,14 @@ def _calc_times():
     """
     Calculates open/close times from miles, using rules
     described at https://rusa.org/octime_alg.html.
-    Expects one URL-encoded argument, the number of miles.
     """
     app.logger.debug("Got a JSON request")
-    km = request.args.get('km', 999, type=float)
-    app.logger.debug("km={}".format(km))
-    app.logger.debug("request.args: {}".format(request.args))
-    # FIXME!
-    # Right now, only the current time is passed as the start time
-    # and control distance is fixed to 200
-    # You should get these from the webpage!
-    open_time = acp_times.open_time(km, 200, arrow.now().isoformat).format('YYYY-MM-DDTHH:mm')
-    close_time = acp_times.close_time(km, 200, arrow.now().isoformat).format('YYYY-MM-DDTHH:mm')
+    km = request.args.get('km', None, type=float)
+    brevet_dist_km = request.args.get('brevet_dist_km', None, type=float)
+    start_time = request.args.get('start_time', arrow.now().isoformat)
+
+    open_time = acp_times.open_time(km, brevet_dist_km, start_time).format('YYYY-MM-DDTHH:mm')
+    close_time = acp_times.close_time(km, brevet_dist_km, start_time).format('YYYY-MM-DDTHH:mm')
     result = {"open": open_time, "close": close_time}
     return flask.jsonify(result=result)
 
